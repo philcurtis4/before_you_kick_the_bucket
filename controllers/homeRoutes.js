5cbc380b94031f867ae27c9eb3b38190c06b95a2
rosey-flo <emilyflo2012@yahoo.com>
2024-08-01 13:25:31 -0400

    server in class checkpoint

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const { route } = require('./api');
+
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    res.render('homepage')
+})
+
+module.exports = router;
