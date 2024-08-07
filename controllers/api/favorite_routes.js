2e59e0b717110e82a9b9252e4075b96a4d40c161
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-08-07 09:32:03 -0400

    finished delete button

--- controllers/api/favorite_routes.js
+++ controllers/api/favorite_routes.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 const { Favorite } = require('../../models');
 
 router.post('/', async (req, res) => {
-	const data = req.body;
-	console.log(req.body);
-	await Favorite.create({
-		country_name: data.country,
-		city_name: data.city,
-		landmark_name: data.landmark,
-		state_name: data.state,
-		userId: req.session.user_id
-	});
+  const data = req.body;
+  console.log(req.body);
+  await Favorite.create({
+    country_name: data.country,
+    city_name: data.city,
+    landmark_name: data.landmark,
+    state_name: data.state,
+    userId: req.session.user_id
+  });
 
-	res.json({
-		message: 'Favorited added.'
-	});
+  res.json({
+    message: 'Favorited added.'
+  });
 });
 
 //   // Set Favorite Rating
@@ -39,18 +39,18 @@ router.post('/', async (req, res) => {
 //   });
 
 // Delete Favorite
-router.delete('/unfav', async (req, res) => {
-	const id = req.body.id;
+router.delete('/unfav/:id', async (req, res) => {
+  const id = req.params.id;
 
-	await Favorite.destroy({
-		where: {
-			id: id
-		}
-	});
+  await Favorite.destroy({
+    where: {
+      id: id
+    }
+  });
 
-	res.json({
-		message: 'Fav removed successfully!'
-	});
+  res.json({
+    message: 'Fav removed successfully!'
+  });
 });
 
 module.exports = router
